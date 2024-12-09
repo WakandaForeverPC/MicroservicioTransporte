ba9ce360d97dc32eb9b418b20622aae2ed67799c
Samuu10 <svallmun@myuax.com>
2024-12-09 10:52:24 +0100

    Initial commit

---
+++ src/main/java/com/proyecto/microserviciotransporte/MicroservicioTransporteApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.microserviciotransporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioTransporteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioTransporteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyecto/microserviciotransporte/MicroservicioTransporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.microserviciotransporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioTransporteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
